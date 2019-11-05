d6261a738ce1a2fb5190d671693a0c0e2051c2f9
Amy Peters <bajagski@gmail.com>
2019-11-05 13:00:10 -0600

    after tutoring

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import datetime as dt
 from dateutil.parser import parse
 
 from flask import Flask, jsonify
+import pandas as pd
+import numpy as np
+import datetime as dt
 
 #################################################
 # Database Setup
@@ -20,6 +23,7 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+session = Session(engine)
 
 #################################################
 # Flask setup
@@ -45,3 +49,44 @@ def welcome():
     )
 
 ##########################################################    
+
+#Convert the query results to a Dictionary using date as the key and prcp as the value.
+#Return the JSON representation of your dictionary.
+
+@app.route("/api/v1.0/precipitation")
+
+def query1():
+
+    last_data_point = session.query(func.max(Measurement.date)).scalar()
+    last_date = dt.datetime.strptime(last_data_point, '%Y-%m-%d')
+    last_date_year = last_date - dt.timedelta(days=365)
+    percip_data = session.query(Measurement.date , Measurement.prcp).filter(Measurement.date > last_date_year).order_by(Measurement.date).all()
+    percip_df = pd.DataFrame(percip_data)
+    perp = percip_df.to_dict()
+            
+    return(jsonify(perp))
+
+@app.route("/api/v1.0/stations")
+
+def query2():
+
+    stations = session.query(Station).count() 
+
+    return(jsonify(stations))
+
+
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+#When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+
+@app.route("/api/v1.0/<start>/<end>")
+
+def calc_temps(start, end):
+
+    temperature = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    #print(temperature)
+    return(jsonify(temperature))
+
+if __name__ == '__main__':
+    app.run(debug=True)
